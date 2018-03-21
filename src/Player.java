952c8f8611eccfeece812bfd7a54a67660701996
rampemus <pasi.toivanen@live.com>
2018-03-21 13:29:18 +0200

    Muokattu periytyvyys toimimaan

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,25 @@
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Input;
 
-public class Player {
-
+public class Player extends Character {
+	public Player(int x, int y) {
+		super(x,y);
+	}
+	
+	public void updateInput(GameContainer gc, int delta) {
+		
+		Input input = gc.getInput();
+		
+		//horozontal movement
+		if ( input.isKeyDown(Input.KEY_D)) {
+			walkRight(delta);
+		} else if ( input.isKeyDown(Input.KEY_A)) {
+			walkLeft(delta);
+		} 
+		
+		//jump mecanic
+		if ( input.isKeyDown(Input.KEY_SPACE) ){
+			jump();
+		}
+	}
 }
