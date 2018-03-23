b8466376405d7b61071528474e2e0a5e749f90ed
Ashtaka97 <tommihheikkinen@gmail.com>
2018-03-23 11:09:48 +0200

    barrel

---
+++ src/Blockade_Barrel.java
@@ -0,0 +1,9 @@
+import org.newdawn.slick.geom.Rectangle;
+
+public class Blockade_Barrel extends Collider{
+//anything at all
+	public Blockade_Barrel() {
+		super(100,100);
+		hitBox = new Rectangle(100, 100, 31, 54);
+	}
+}
--- src/MOB.java
+++
@@ -1,4 +0,0 @@
-
-public class MOB {
-//anything at all
-}
