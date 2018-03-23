86530c60b651daa9a87faec543e5b844318a8531
Ashtaka97 <tommihheikkinen@gmail.com>
2018-03-23 13:21:08 +0200

    mor barrel

--- src/Blockade_Barrel.java
+++ src/Blockade_Barrel.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.state.StateBasedGame;
 
 public class Blockade_Barrel extends Character implements Visible, Active{
 //anything at all
@@ -8,5 +13,8 @@ public class Blockade_Barrel extends Character implements Visible, Active{
 		super(100,100);
 		hitBox = new Rectangle(100, 100, 31, 54);
 	}
-
+	public void attack(int delta) {
+			walkRight(delta);
+	}
 }
+
