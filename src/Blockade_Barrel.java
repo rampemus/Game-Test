1437877846541ade4da839b1bfcc36cb8652729c
Ashtaka97 <tommihheikkinen@gmail.com>
2018-03-23 12:24:47 +0200

    enemy test & stuff

--- src/Blockade_Barrel.java
+++ src/Blockade_Barrel.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
+
 import org.newdawn.slick.geom.Rectangle;
 
-public class Blockade_Barrel extends Collider{
+public class Blockade_Barrel extends Collider implements Visible{
 //anything at all
 	public Blockade_Barrel() {
 		super(100,100);
 		hitBox = new Rectangle(100, 100, 31, 54);
+	}
+	@Override
+	public void update(ArrayList<Object> o, Map m, int delta) {
+		o.add(new Blockade_Barrel());
 	}
 }
---
+++ src/Enemy_Test.java
@@ -0,0 +1,33 @@
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Enemy_Test extends StateBasedGame {
+	
+	public Enemy_Test(String name) {
+		super(name);
+	}
+
+	public static void main(String[] args) {
+
+		AppGameContainer appgc;
+		
+		try {
+			appgc = new AppGameContainer(new Main("Collider-luokan testaukseen"));
+			appgc.setDisplayMode(800, 600, false);
+			appgc.setAlwaysRender(true);
+			appgc.start();
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void initStatesList(GameContainer gc) throws SlickException {
+		addState(new Emptygame());
+	}
+
+}
+
