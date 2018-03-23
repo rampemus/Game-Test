6b8f9924580d6e88fbe58a4659fd110b87682c92
rampemus <pasi.toivanen@live.com>
2018-03-23 13:39:36 +0200

    Charactereille ampumis-metodi

--- src/Character.java
+++ src/Character.java
@@ -1,14 +1,35 @@
+import java.util.ArrayList;
+
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Vector2f;
 
 public class Character extends Collider{
 	protected float xAcceleration = 0.005f;
+	protected int shootCooldown = 0;
 	
 	public Character(int x, int y) {
 		super(x,y);
 		height = 64; //resize hitbox
 		width = 32;
 		hitBox = new Rectangle(0, 0, width, height);
+	}
+	
+	public void update(ArrayList<Object> o, Map m, int delta) {
+		super.update(o, m, delta);
+		
+		if ( shootCooldown > 0) {
+			shootCooldown -= delta;
+		} else {
+			shootCooldown = 0;
+		}
+	}
+	
+	public void shoot() {
+		if (shootCooldown == 0) {
+			// bulletin luominen
+			//animaation vaihtaminen ampumiseen jne.
+			//shootCooldown = weaponArray.get(joku).fireRate
+		}
 	}
 	
 	public void walkRight(int delta) {
