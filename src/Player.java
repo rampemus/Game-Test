7560a14dd8f46bd03029169c4c6a2014d32ba6d4
Santtinen <santtinen3@gmail.com>
2018-04-10 16:28:31 +0300

    Fix

--- src/Player.java
+++ src/Player.java
@@ -99,8 +99,7 @@ public class Player extends Character {
 		v.sub(p);
 		v.add(getMouse());
 		r.setLocation(this.getX(), this.getY());
-		r = r.transform(Transform.createRotateTransform((float)v.getTheta() * 0.01745329252f - 1.57079632679f, this.getX(), this.getY()));
-		g.draw((Shape)r);
+		g.draw((Shape)r.transform(Transform.createRotateTransform((float)v.getTheta() * 0.01745329252f - 1.57079632679f, this.getX(), this.getY())));
 		character.getFlippedCopy(!lookingRight, false).draw(this.getX()-width/2-16,this.getY()-height/2);
 	}
 	
@@ -136,6 +135,6 @@ public class Player extends Character {
 	}
 	
 	public Shape getLineOfFire() {
-		return r;
+		return (Shape)r.transform(Transform.createRotateTransform((float)v.getTheta() * 0.01745329252f - 1.57079632679f, this.getX(), this.getY()));
 	}
 }
