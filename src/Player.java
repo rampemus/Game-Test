22e093e0b858bf74f162ac1b92f6bed65eb15396
Santtinen <santtinen3@gmail.com>
2018-04-10 16:35:04 +0300

    Commented line of fire visibility

--- src/Player.java
+++ src/Player.java
@@ -99,7 +99,7 @@ public class Player extends Character {
 		v.sub(p);
 		v.add(getMouse());
 		r.setLocation(this.getX(), this.getY());
-		g.draw((Shape)r.transform(Transform.createRotateTransform((float)v.getTheta() * 0.01745329252f - 1.57079632679f, this.getX(), this.getY())));
+		//g.draw((Shape)r.transform(Transform.createRotateTransform((float)v.getTheta() * 0.01745329252f - 1.57079632679f, this.getX(), this.getY())));
 		character.getFlippedCopy(!lookingRight, false).draw(this.getX()-width/2-16,this.getY()-height/2);
 	}
 	
