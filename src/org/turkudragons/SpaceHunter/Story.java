aaa1077aa5d10861f3ac822054b42e302d728c73
rampemus <pasi.toivanen@live.com>
2018-04-15 00:00:55 +0300

    Story on nyt kokopitkä

--- src/org/turkudragons/SpaceHunter/Story.java
+++ src/org/turkudragons/SpaceHunter/Story.java
@@ -11,14 +11,32 @@ public class Story extends BasicGameState{
 
 	public static final int id = 6;
 	Input input;
-	String[] story = {"Date: 20XX, March 6th",
+	String[] story = {	"Date: 20XX, March 6th",
 						"Location: Orbiting TLX-278 (Sigmar System)",
 						"Weather: 17,2% chance of meteor showers, 42,8% chance for UV-burst",
 						"Entry: After 3 months of silence, I received my first job request in a while, quite",
 						" a big one at that. Assignment of slaying several important targets across the ",
 						"system, with cash payment after finishing! The price is high enough to warrant ",
 						"an early retirement for anybody, and their gramma. It would be stupid ",
-						"not to accept this one. Besides, my gramma really needs a new swimming pool."};
+						"not to accept this one. Besides, my gramma really needs a new swimming pool.",
+						"",
+						"Date: 20XX, March 9th\n",
+						"Location: Asteroid belt between Sigmar & Machina systems\n",
+						"Weather: 100% meteors & occasional warships\n",
+						"Entry: Moving on to the location “Classified” to begin mission. These targets ",
+						"include monsters, political figures, criminals etc. No wonder they’re wanted dead.",
+						" Lucky for them, they hired the right man for the job, my glorious reputation ",
+						"exceeds me! ",
+						"I only wish there’s something more than just monsters in these space rocks, ",
+						"without a good restaurant how will I be able to keep up my perfect diet?",
+						"",
+						"Date: 20XX, March 12th\n", 
+						"Location: Classified\n" ,
+						"Weather: Lots of aliens\n",
+						"Entry: Beginning mission, estimated time of completion minimal, failures are ",
+						"unacceptable, as my name knows no defeat, no challenge that’s too great and no danger",
+						"I cannot face! Ancis Bastion Rexas Diablo Frey XXI, moving out!"
+						};
 	
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
 		input = gc.getInput();
@@ -27,7 +45,7 @@ public class Story extends BasicGameState{
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		g.drawString("(PRESS ESC TO RETURN)", 140, 10);
 		for ( int i = 0; i < story.length; i++) {
-			g.drawString(story[i], 10, 120 + 20*i);
+			g.drawString(story[i], 10, 60 + 20*i);
 		}
 	}
 
